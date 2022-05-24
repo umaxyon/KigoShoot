027a61afcdda0b22099042cd180a11a61c708726
Umaxyon <umaxyon@gmail.com>
2022-05-24 19:29:30 +0900

    README更新

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # KigoShoot
+
+![kigo](https://user-images.githubusercontent.com/73870939/170010730-117dcaa0-45b6-4d3d-99cf-f80579448557.gif)
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsInlineLimit: 0
+    assetsInlineLimit: 0 // 画像のインライン化抑止
   }
 })
